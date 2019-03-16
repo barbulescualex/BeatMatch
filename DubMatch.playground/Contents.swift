d8a41bd5945eeddb7c38eae010f2ebe1adabb345
Alex Barbulescu <alexbarbulescu01@gmail.com>
2019-03-15 20:51:43 -0400

    Initial init

---
+++ DubMatch.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+class MyViewController : UIViewController {
+    override func loadView() {
+        let view = UIView()
+        view.backgroundColor = .white
+
+        let label = UILabel()
+        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
+        label.text = "Hello World!"
+        label.textColor = .black
+        
+        view.addSubview(label)
+        self.view = view
+    }
+}
+// Present the view controller in the Live View window#imageLiteral(resourceName: "Screen Shot 2019-03-13 at 1.00.02 PM.png")
+PlaygroundPage.current.liveView = MyViewController()
