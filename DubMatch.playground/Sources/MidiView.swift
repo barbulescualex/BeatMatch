138c1dc6f78bfb60aa39b0b884165c4a99097e15
Alex Barbulescu <alexbarbulescu01@gmail.com>
2019-03-16 21:59:11 -0400

    drag and drop done

--- DubMatch.playground/Sources/MidiView.swift
+++ DubMatch.playground/Sources/MidiView.swift
@@ -3,8 +3,9 @@ import PlaygroundSupport
 
 public class MidiView : UIView {
     private var identifier = "cell"
+    private var sounds = [Sounds.bass,Sounds.snare,Sounds.ghostSnare,Sounds.chime,Sounds.hiHat]
     
-    lazy var collectionView : UICollectionView = {
+    private lazy var collectionView : UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 1
         layout.minimumInteritemSpacing = 1
@@ -12,6 +13,7 @@ public class MidiView : UIView {
         collectionView.backgroundColor = .clear
         collectionView.showsVerticalScrollIndicator = false
         collectionView.showsHorizontalScrollIndicator = false
+        collectionView.isScrollEnabled = false
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(MidiCell.self, forCellWithReuseIdentifier: identifier)
@@ -40,14 +42,45 @@ public class MidiView : UIView {
         addSubview(collectionView)
         collectionView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10).isActive = true
         collectionView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10).isActive = true
-        collectionView.topAnchor.constraint(equalTo: topAnchor, constant: 10).isActive = true
+        collectionView.topAnchor.constraint(equalTo: topAnchor, constant: 20).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -10).isActive = true
+        
+        let longPress = UILongPressGestureRecognizer(target: self, action: #selector(handleMoveGesture(_:)))
+        collectionView.addGestureRecognizer(longPress)
+        
+        //label
+//        let label = UILabel()
+//        label.text = "WWDC 2019 Midi Controller"
+//        label.textColor = .black
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        
+//        addSubview(label)
+//        label.topAnchor.constraint(equalTo: topAnchor, constant: -2).isActive = true
+//        label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2).isActive = true
+    }
+    
+    @objc func handleMoveGesture(_ sender : UILongPressGestureRecognizer){
+        switch(sender.state) {
+        case .began:
+            guard let selectedIndexPath = collectionView.indexPathForItem(at: sender.location(in: collectionView)) else {
+                break
+            }
+            collectionView.beginInteractiveMovementForItem(at: selectedIndexPath)
+        case .changed:
+            collectionView.updateInteractiveMovementTargetPosition(sender.location(in: sender.view!))
+        case .ended:
+            collectionView.endInteractiveMovement()
+        default:
+            collectionView.cancelInteractiveMovement()
+        }
+
     }
     
 }
 
 extension MidiView : UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-
+    
+    //population
     public func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
@@ -58,20 +91,32 @@ extension MidiView : UICollectionViewDataSource, UICollectionViewDelegate, UICol
     
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! MidiCell
-        cell.sound = Sounds.make(index: indexPath.item)
+        cell.sound = sounds[indexPath.item]
         return cell
     }
     
+    //selection
     public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         guard let cell = collectionView.cellForItem(at: indexPath) as? MidiCell else {return}
         cell.animate()
         cell.playSound()
     }
     
+    //sizing
     public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let side = collectionView.bounds.width / 3 - 10
         return CGSize(width: side, height: side)
     }
     
+    //moving
+    public func collectionView(_ collectionView: UICollectionView, canMoveItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    public func collectionView(_ collectionView: UICollectionView, moveItemAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let soundToMove = sounds[sourceIndexPath.item]
+        sounds.remove(at: sourceIndexPath.item)
+        sounds.insert(soundToMove, at: destinationIndexPath.item)
+    }
 }
 
